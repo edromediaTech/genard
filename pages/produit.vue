4d01ede5c2d7c3eff90b0010dea27568f6cc176f
Djenica Rubes <djenicarubes@gmail.com>
2025-01-17 15:01:52 -0500

    produit

--- pages/produit.vue
+++ pages/produit.vue
@@ -4,7 +4,98 @@
     <v-form v-model="valid" @submit.prevent="submitForm">
       <v-card v-if="isAdmin" class="px-4">
         <v-row class="mt-4">
-          <!-- ... (votre formulaire existant) ... -->
+          <!-- Nom du produit -->
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field
+              v-model="produit.nom"
+              label="Nom du produit"
+              required
+            ></v-text-field>
+          </v-col>
+  
+          <!-- Catégorie du produit -->
+          <v-col cols="12" sm="6" md="4">
+            <v-select
+              v-model="produit.categorie"
+              :items="categoriesOptions"
+              label="Catégorie"
+              required
+            ></v-select>
+          </v-col>
+  
+          <!-- Quantité du produit -->
+          <v-col cols="12" sm="6" md="2">
+            <v-text-field
+              v-model="produit.quantite"
+              label="Quantité"
+              type="number"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <!-- Prix du produit -->
+          <v-col cols="12" sm="6" md="2">
+            <v-text-field
+              v-model="produit.prix"
+              label="Prix"
+              type="number"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <!-- Champs supplémentaires -->
+          <v-col cols="12" sm="6" md="3">
+            <v-text-field
+              v-model="produit.ramplacement"
+              label="Remplacés"
+              type="number"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="3">
+            <v-text-field
+              v-model="produit.defectue"
+              label="Défectueux"
+              type="number"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="3">
+            <v-text-field
+              v-model="produit.critique"
+              label="Critique"
+              type="number"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col cols="12" sm="6" md="3">
+            <v-text-field
+              v-model="produit.alerte"
+              label="Alerte"
+              type="number"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <!-- Boutons d'action -->
+          <v-col cols="12" class="d-flex justify-end">
+            <v-btn 
+              :disabled="!produit.nom || !produit.categorie" 
+              color="primary" 
+              class="mr-2"
+              @click="submitForm"
+            >
+              {{ isEditing ? 'Modifier' : 'Ajouter' }}
+            </v-btn>
+            <v-btn 
+              v-if="isEditing" 
+              color="secondary" 
+              @click="clearForm"
+            >
+              Annuler
+            </v-btn>
+          </v-col>
+  
         </v-row>
       </v-card>
     </v-form>
@@ -86,7 +177,7 @@
         <v-btn v-if="isAdmin" icon @click="editProduit(item)">
           <v-icon>mdi-pencil</v-icon>
         </v-btn>
-        <v-btn v-if="isAdmin" icon @click="deleteProduit(item.id)">
+        <v-btn v-if="isAdmin" icon @click="deleteProduit(item._id)">
           <v-icon>mdi-delete</v-icon>
         </v-btn>
       </template>
